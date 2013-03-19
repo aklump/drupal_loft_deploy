5dae7d14875c6159f9e941c64ee9efa98e4cccb5
aklump <sourcecode@intheloftstudios.com>
2013-03-19 14:39:03 -0700

    Ported to Drupal 5 from Drupal 7

--- loft_deploy.js
+++ loft_deploy.js
@@ -6,31 +6,9 @@
  * @ingroup loft_deploy
  * @{
  */
-
-(function ($) {
-
-  Drupal.loftDeploy = Drupal.loftDeploy || {};
-
-  //Drupal.loftDeploy.someVariable = Drupal.loftDeploy.someVariable || {};
-  //Drupal.loftDeploy.someVariable = value;
-
-  //Drupal.loftDeploy.someFunction = function() {
-  //
-  //}
-
-  /**
-  * Core behavior for loft_deploy.
-  */
-  Drupal.behaviors.loftDeploy = Drupal.behaviors.loftDeploy || {};
-  Drupal.behaviors.loftDeploy.attach = function (context, settings) {
-    var $toggle = $('.loft-deploy .hide');
-    $toggle.click(function() {
-      $('.loft-deploy').fadeOut();
-    });
-  }
-
-  /**
-  * @} End of "defgroup loft_deploy".
-  */
-
-})(jQuery);
+$(document).ready(function() {
+  var $toggle = $('.loft-deploy .hide');
+  $toggle.click(function() {
+    $('.loft-deploy').fadeOut();
+  });
+});
