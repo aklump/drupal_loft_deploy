f1237409202e7cbdb1d82901561f899f5f3861ac
aklump <sourcecode@intheloftstudios.com>
2014-01-27 15:46:07 -0800

    added pre/post title callbacks

---
+++ loft_deploy.api.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file
+ * API catalog
+ *
+ * @ingroup loft_deploy
+ * @{
+ */
+
+/**
+ * Implements hook_loft_deploy_title_pre_alter().
+ */
+function hook_loft_deploy_title_pre_alter(&$title) {
+  // @todo alter the title string  
+}
+
+/**
+ * Implements hook_loft_deploy_title_post_alter().
+ */
+function hook_loft_deploy_title_post_alter(&$title) {
+  // @todo alter the title string  
+}
