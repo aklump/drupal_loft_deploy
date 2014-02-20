fa0a76099994410f0fb2f635da06384ab79df525
aklump <sourcecode@intheloftstudios.com>
2014-02-20 11:03:31 -0800

    Added $.cookie and fixed bug introduced recently related to cookies.

--- loft_deploy.js
+++ loft_deploy.js
@@ -29,7 +29,6 @@
     // Single click hides until next page load
     $toggle.click(function(e) {
 
-
       // Was the meta key held down? Set cookie?
       if (e.metaKey) {
         $('.loft-deploy').fadeOut(function() {
@@ -38,17 +37,18 @@
           });
         });
 
+        $.cookie('loft_deploy', )
+
         // Cookie handling
         var c_name = 'loft_deploy';
         var expiry = new Date();
         var time = expiry.getTime();
-
-        // Set a cookie if user asks us to
         time += duration * 60 * 1000;
         expiry.setTime(time);
         var c_value = $(this).attr('class').match(/hidden/);
-        c_value = escape(c_value) + ((expiry === null) ? "" : "; expires=" + expiry.toGMTString());
-        document.cookie = c_name + "=" + c_value;
+        $.cookie(c_name, c_value, {
+          expires: expiry.toGMTString()
+        });  
       }
       else {
         $('.loft-deploy').fadeOut();
