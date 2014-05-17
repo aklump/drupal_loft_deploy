cf37a3121f126ffa0352f9688b9b4aafca9062fc
aklump <sourcecode@intheloftstudios.com>
2014-05-16 20:28:01 -0700

    Added loft_deploy_ip module.

--- loft_deploy.api.php
+++ loft_deploy.api.php
@@ -20,3 +20,14 @@ function hook_loft_deploy_title_pre_alter(&$title) {
 function hook_loft_deploy_title_post_alter(&$title) {
   // @todo alter the title string  
 }
+
+/**
+ * Implements hook_loft_deploy_border_access().
+ *
+ * @param  bool $access Set to TRUE to show the border or FALSE to hide.
+ * @param  string $site_role
+ */
+function hook_loft_deploy_border_access_alter(&$access, $site_role) {
+  // @todo alter the access based on something.
+  $access = FALSE;
+}
