a31178d0089c145bdfe4e720514956906f41d0d3
aklump <sourcecode@intheloftstudios.com>
2014-06-28 07:56:49 -0700

    Added color support for feature branches: dev and staging.  Updated .api docs.

--- loft_deploy.api.php
+++ loft_deploy.api.php
@@ -9,6 +9,13 @@
 
 /**
  * Implements hook_loft_deploy_title_pre_alter().
+ *
+ * Use this if to change the title BEFORE dynamic text is applied.  This list
+ * is the dynamic text:
+ *
+ * - !site_role
+ * - !git_branch
+ * - !gitflow
  */
 function hook_loft_deploy_title_pre_alter(&$title) {
   // @todo alter the title string  
@@ -16,9 +23,14 @@ function hook_loft_deploy_title_pre_alter(&$title) {
 
 /**
  * Implements hook_loft_deploy_title_post_alter().
+ *
+ * Use this if you just want the change the title and don't care about tokens,
+ * or if you want to alter it after the tokens have been replaced.
  */
 function hook_loft_deploy_title_post_alter(&$title) {
-  // @todo alter the title string  
+  if ($_SERVER['SERVER_NAME'] === 'beta.intheloftstudios.com') {
+    $title = t('InTheLoftStudios.com "BETA". Use by permission only.');
+  }  
 }
 
 /**
