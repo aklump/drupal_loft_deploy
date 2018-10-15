5b7a519fb66c675114e72b6ef6cbbe74c02ead80
Aaron Klump <sourcecode@intheloftstudios.com>
2018-10-14 18:06:19 -0700

    add missing image

--- .web_package/hooks/build/00_compile_docs.sh
+++ .web_package/hooks/build/00_compile_docs.sh
@@ -9,13 +9,14 @@ test -f "$7/docs/index.html" || exit 1
 ## Copy screenshot.
 test -d "$7/images" || mkdir -p "$7/images"
 cp "$7/documentation/source/images/screenshot.png" "$7/images/screenshot.png" || exit 1
+cp "$7/documentation/source/images/by-ip.png" "$7/images/by-ip.png" || exit 1
 
 ## Auto commit the files generated as output.
 git=$(type git >/dev/null 2>&1 && which git)
 if test -d $7/.git && [ "$git" ]; then
     # Note to support symlinks, we should cd first (per git).
     (cd $7/docs && git add .)
-    (cd $7 && git add README.md && git add images/screenshot.png)
+    (cd $7 && git add README.md && git add images)
 fi
 
 exit 0
